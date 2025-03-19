3168e8c40635ccd134b042cfea2f95962eeef50d
Deepam Kapur <deepamkapur@gmail.com>
2025-03-19 15:45:20 +0530

    [UPDATE] Add timers to the auth flow

--- src/api/public/controllers/auth.controller.ts
+++ src/api/public/controllers/auth.controller.ts
@@ -8,24 +8,43 @@ import { getAuthUserByUUID, upsertAuthUser } from '@/database/repositories/AuthU
 import { generateToken } from '@/utils/jwt.utils';
 
 const token = async (req: Request, res: Response): Promise<void> => {
+  const startTime = performance.now(); // Start timer for the whole function
+
   const { authCode, redirectURI } = req.body as {
     authCode: string
     redirectURI: string
   };
 
+  const linkedinAccessTokenStartTime = performance.now();
   const authData: ILinkedinAccessTokenResponse | null = await authUtils.linkedinAccessToken(authCode, redirectURI);
+  const linkedinAccessTokenEndTime = performance.now();
+  console.log(`linkedinAccessToken took: ${linkedinAccessTokenEndTime - linkedinAccessTokenStartTime}ms`);
+
   if (!authData) {
     throw new Error('Invalid user');
   }
 
+  const upsertAuthUserStartTime = performance.now();
   const authUser = await upsertAuthUser(authData);
+  const upsertAuthUserEndTime = performance.now();
+  console.log(`upsertAuthUser took: ${upsertAuthUserEndTime - upsertAuthUserStartTime}ms`);
+
   if (!authUser) {
     throw new Error('Invalid user');
   }
 
-  const token = generateToken(authUser.authUserId);
+  const generateTokenStartTime = performance.now();
+  const tokenValue = generateToken(authUser.authUserId);
+  const generateTokenEndTime = performance.now();
+  console.log(`generateToken took: ${generateTokenEndTime - generateTokenStartTime}ms`);
 
-  responseUtils.success(res, { token, status: authUser.status, firstName: authUser.firstName, lastName: authUser.lastName });
+  const responseUtilsStartTime = performance.now();
+  responseUtils.success(res, { token: tokenValue, status: authUser.status, firstName: authUser.firstName, lastName: authUser.lastName });
+  const responseUtilsEndTime = performance.now();
+  console.log(`responseUtils.success took: ${responseUtilsEndTime - responseUtilsStartTime}ms`);
+
+  const endTime = performance.now(); // End timer for the whole function
+  console.log(`token function took: ${endTime - startTime}ms`);
 };
 
 const status = async (req: Request, res: Response): Promise<void> => {
